e7859162839a8056ae3787ccf1a0f3e37a8c3228
Chahat Kalsi <chahat.ck88@gmail.com>
2024-11-02 13:17:40 -0400

    Add project files.

---
+++ VTK_Engine/VTK_Engine.cpp
@@ -0,0 +1,19 @@
+﻿// VTK_Engine.cpp : Defines the entry point for the application.
+//
+
+#include "VTK_Engine.h"
+#include "vtkActor.h"
+#include "vtkNew.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	cout << "Hello VTK." << endl;
+
+	vtkNew<vtkActor> actor;
+	actor->Print(cout);
+
+	return 0;
+}
